f1ee9c16caf234f7282b94a7e092fb39bf99c614
Crayo0n <mauricio28rd@gmail.com>
2026-03-25 10:18:48 -0600

    Corrección de nombre de BD

--- FastAPI/app/db/database.py
+++ FastAPI/app/db/database.py
@@ -6,7 +6,7 @@ import os
 # 1. Definiendo la URL de conexion
 DATABASE_URL = getattr(settings, "DATABASE_URL", os.getenv(
     "DATABASE_URL", 
-    "postgresql://admin:123456@postgres:5432/DB_miapi"
+    "postgresql://admin:123456@postgres:5432/DB_Priority_Pulse"
 ))
 
 # 2. Creamos el motor de conexion
